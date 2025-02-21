c7b796668dfa10f0853d196e3e1e3ad6ea964d8b
alexiynew <lehafeda@gmail.com>
2025-02-21 12:36:44 +0300

    Add gtest

--- libs/glm.cmake
+++ libs/glm.cmake
@@ -4,6 +4,7 @@ FetchContent_Declare(
 	GLM
 	GIT_REPOSITORY https://github.com/g-truc/glm.git
 	GIT_TAG        1.0.1
+	PATCH_COMMAND ${CMAKE_COMMAND} -P ${CMAKE_SOURCE_DIR}/libs/glm_patch.cmake ${CMAKE_SOURCE_DIR}/build/_deps/glm-src
 )
 
 set(GLM_ENABLE_CXX_17 ON CACHE BOOL "" FORCE)
--- libs/gtest.cmake
+++ libs/gtest.cmake
@@ -1,36 +1,22 @@
 include(FetchContent)
 
 FetchContent_Declare(
-    googletest
+    googletest # Due to some bug in FetchContent scripts name of dependency must be exactly "googletest"
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG        v1.15.2
+    GIT_TAG        v1.16.0
 )
 
 set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
 
-set(GTEST_CUSTOM_GCC_OPTIONS -Wno-undef)
-
-add_library(gtest_custom_compile_options INTERFACE)
-target_compile_options(gtest_custom_compile_options 
-    INTERFACE 
-        $<${GCC_LIKE_COMPILER}:${GTEST_CUSTOM_GCC_OPTIONS}>
-        $<${MSVC_LIKE_COMPILER}:${GTEST_CUSTOM_MSVC_OPTIONS}>
-)
-
 FetchContent_MakeAvailable(googletest)
-
-target_link_libraries(gtest PRIVATE gtest_custom_compile_options)
-target_link_libraries(gtest_main PRIVATE gtest_custom_compile_options)
-target_link_libraries(gmock PRIVATE gtest_custom_compile_options)
-target_link_libraries(gmock_main PRIVATE gtest_custom_compile_options)
 
 set_target_properties(gtest gtest_main gmock gmock_main PROPERTIES FOLDER "libs/gtest")
 
 # Define lids to link
-add_library(Libs::GTest INTERFACE IMPORTED)
-target_link_libraries(Libs::GTest INTERFACE gtest_main)
-target_include_directories(Libs::GTest INTERFACE ${googletest_SOURCE_DIR}/googletest/include)
+add_library(Libs::gtest INTERFACE IMPORTED)
+target_link_libraries(Libs::gtest INTERFACE gtest_main)
+target_include_directories(Libs::gtest INTERFACE ${googletest_SOURCE_DIR}/googletest/include)
 
-add_library(Libs::GMock INTERFACE IMPORTED)
-target_link_libraries(Libs::GMock INTERFACE gmock_main)
-target_include_directories(Libs::GMock INTERFACE ${googletest_SOURCE_DIR}/googletest/include)
+add_library(Libs::gmock INTERFACE IMPORTED)
+target_link_libraries(Libs::gmock INTERFACE gmock_main)
+target_include_directories(Libs::gmock INTERFACE ${googletest_SOURCE_DIR}/googlemock/include)
