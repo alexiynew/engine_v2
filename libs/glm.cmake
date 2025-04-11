7f18f64bfc0d634e897c3dd763f102e684f67552
CruiserChapaev <108793087+CruiserChapaev@users.noreply.github.com>
2025-04-11 11:05:34 +0300

    Wuzzup#2 (#3)
    * + Added CMAKE_PRESET_NAME to cmake flags
    + Fixed error with GLM patch script under windows
    + Added some cool presets for best gcc bydlocoding experience under skuf platforms
    
    * Works fine now, but still problems with gtest lib on linking step
    
    * Review changes

--- libs/glm.cmake
+++ libs/glm.cmake
@@ -4,13 +4,20 @@ FetchContent_Declare(
 	GLM
 	GIT_REPOSITORY https://github.com/g-truc/glm.git
 	GIT_TAG        1.0.1
-	PATCH_COMMAND ${CMAKE_COMMAND} -P ${CMAKE_SOURCE_DIR}/libs/glm_patch.cmake ${CMAKE_SOURCE_DIR}/build/_deps/glm-src
 )
 
 set(GLM_ENABLE_CXX_17 ON CACHE BOOL "" FORCE)
 set(GLM_ENABLE_CXX_20 ON CACHE BOOL "" FORCE)
 
 FetchContent_MakeAvailable(GLM)
+
+FetchContent_GetProperties(GLM)
+if(GLM_POPULATED)
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_SOURCE_DIR}/${BUILD_PRESET_NAME}/libs/glm_patch.cmake ${GLM_SOURCE_DIR}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+endif()
 
 set(GLM_GCC_CUSTOM_COMPILE_OPTIONS -Wno-unsafe-buffer-usage)
 set(GLM_MSVC_CUSTOM_COMPILE_OPTIONS )
