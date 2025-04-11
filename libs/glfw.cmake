71aa5287d2b0a80920c86506e8ac9c4158069683
alexiynew <lehafeda@gmail.com>
2025-04-11 21:47:20 +0300

    Refactor some build scripts

--- libs/glfw.cmake
+++ libs/glfw.cmake
@@ -1,16 +1,16 @@
 include(FetchContent)
 
 FetchContent_Declare(
-    GLFW
+    glfw
     GIT_REPOSITORY https://github.com/glfw/glfw.git
     GIT_TAG        3.4
 )
 
-FetchContent_MakeAvailable(GLFW)
+FetchContent_MakeAvailable(glfw)
 
 set_target_properties(glfw uninstall update_mappings PROPERTIES FOLDER "libs/glfw3")
 
 # Define lids to link
 add_library(Libs::glfw INTERFACE IMPORTED)
 target_link_libraries(Libs::glfw INTERFACE glfw)
-target_include_directories(Libs::glfw INTERFACE ${GLFW_SOURCE_DIR}/include)
+target_include_directories(Libs::glfw INTERFACE ${glfw_SOURCE_DIR}/include)
